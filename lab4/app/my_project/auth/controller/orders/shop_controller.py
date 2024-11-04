22ecbacf49ad8b43a058215cb01ab154bb81fb32
vladkuz54 <vkuz1540@gmail.com>
2024-11-04 20:32:21 +0200

    Update shop_controller.py

--- lab4/app/my_project/auth/controller/orders/shop_controller.py
+++ lab4/app/my_project/auth/controller/orders/shop_controller.py
@@ -1,5 +1,3 @@
-from typing import List, Dict, Any
-
 from lab4.app.my_project.auth.service import shop_service
 from lab4.app.my_project.auth.controller.general_controller import GeneralController
 
@@ -7,5 +5,3 @@ from lab4.app.my_project.auth.controller.general_controller import GeneralContro
 class ShopController(GeneralController):
     _service = shop_service
 
-    def get_shops_after_client(self, client_id: int) -> List[object]:
-        return self._service.get_shops_after_client(client_id)
