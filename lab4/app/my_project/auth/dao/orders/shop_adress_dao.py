6b90c50718af007cf073477976a5fb73a4aa560c
vladkuz54 <vkuz1540@gmail.com>
2024-11-04 20:32:25 +0200

    Update shop_adress_dao.py

--- lab4/app/my_project/auth/dao/orders/shop_adress_dao.py
+++ lab4/app/my_project/auth/dao/orders/shop_adress_dao.py
@@ -1,11 +1,6 @@
-from typing import List
-
 from lab4.app.my_project.auth.dao.general_dao import GeneralDAO
 from lab4.app.my_project.auth.domain import Shop_Adress
 
 class ShopAdressDAO(GeneralDAO):
 
     _domain_type = Shop_Adress
-
-    def find_by_street(self, street: str) -> List[object]:
-        return self._session.query(Shop_Adress).filter(Shop_Adress.street, Shop_Adress.street_number == street).all()
