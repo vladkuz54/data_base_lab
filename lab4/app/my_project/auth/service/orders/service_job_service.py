89bbdb7700b3507ec796f7b397c199de7e8fc4dc
vladkuz54 <vkuz1540@gmail.com>
2024-11-08 14:39:34 +0200

    Update service_job_service.py

--- lab4/app/my_project/auth/service/orders/service_job_service.py
+++ lab4/app/my_project/auth/service/orders/service_job_service.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from lab4.app.my_project.auth.dao import service_job_dao
-from lab4.app.my_project.auth.domain.orders.service_job import ServiceJob
 from lab4.app.my_project.auth.service.general_service import GeneralService
 
 
@@ -11,3 +10,6 @@ class ServiceJobService(GeneralService):
 
     def get_service_jobs_after_service_type(self, service_type_id: int) -> List[object]:
         return self._dao.get_service_jobs_after_service_type(service_type_id)
+
+    def get_service_jobs_after_terminal(self, terminal_id: int) -> List[object]:
+        return self._dao.get_service_jobs_after_terminal(terminal_id)
